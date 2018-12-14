b9e55d49f7470fd9d0d36b7489f73967503cc886
Miklos Marton <martonmiklosqdev@gmail.com>
2018-12-14 16:18:29 +0100

    Initial add

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: mm
+ *
+ * Created on 2018. december 14., 16:15
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
